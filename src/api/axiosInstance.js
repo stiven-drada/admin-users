8164dcc237f0f14cdf5c3b493b6dea9a54f43ee4
stiven-drada <stivendrada@hotmail.com>
2023-07-02 20:40:18 -0500

    se cambio la url de la api para entregable

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://users-crud.academlo.tech/",
+  baseURL: "https://user-crud-n3hx.onrender.com",
 });
+
+//https://users-crud.academlo.tech/
