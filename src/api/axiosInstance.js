762b331c8bbab6ad37abdec12c18fb7495242832
stiven-drada <stivendrada@hotmail.com>
2023-07-07 11:29:42 -0500

    se cambio la url de la api

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://user-crud-n3hx.onrender.com",
+  baseURL: "https://users-crud.academlo.tech/",
 });
-
-//https://users-crud.academlo.tech/
